74e8855dc326fb63459a4e71bfbfb2b9b96f42a6
mohican16 <faceitall@yandex.ru>
2018-06-05 20:27:42 +0300

    Update bullhead fingerprint to 8.1.0 (OPM6.171019.030.B1, Jun 2018)

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM4.171019.016.A1/4720815:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM4.171019.016.A1 4720815 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.B1 4768815 release-keys"
